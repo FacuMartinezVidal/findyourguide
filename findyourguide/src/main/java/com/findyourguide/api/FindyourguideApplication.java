9914ca7c93ecbc2b40a2f839c6a057611661af28
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2024-06-02 23:59:31 -0300

    create project

---
+++ findyourguide/src/main/java/com/findyourguide/api/FindyourguideApplication.java
@@ -0,0 +1,13 @@
+package com.findyourguide.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindyourguideApplication.class, args);
+    }
+
+}
---
+++ findyourguide/src/test/java/com/findyourguide/api/FindyourguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findyourguide.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyourguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
