2f13a69d917beb1fb74d288916d1b84a44455a15
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2024-06-11 19:37:00 -0300

    fix response

--- api/src/main/java/com/findyourguide/api/controller/AuthController.java
+++ api/src/main/java/com/findyourguide/api/controller/AuthController.java
@@ -2,41 +2,40 @@ package com.findyourguide.api.controller;
 
 
 import com.findyourguide.api.dto.LoginDTO;
-import com.findyourguide.api.dto.RegisterGuideDTO;
-import com.findyourguide.api.dto.RegisterTouristDTO;
+import com.findyourguide.api.dto.RegisterDTO;
+import com.findyourguide.api.dto.ResponseDTO;
 import com.findyourguide.api.service.AuthServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Validated
 public class AuthController {
 
     final AuthServiceImpl authService;
 
     @PostMapping("/login/{type}")
-    public ResponseEntity<String> login(@PathVariable String type, @RequestBody LoginDTO request) {
+    public ResponseEntity<ResponseDTO> login(@PathVariable String type, @Valid @RequestBody LoginDTO request) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authService.login(request, type));
-        return ResponseEntity.status(HttpStatus.OK).headers(headers).body("Successfully logged in");
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(new ResponseDTO(200,"success"));
 
     }
 
-    @PostMapping("/register/tourist")
-    public ResponseEntity<String> register(@RequestBody RegisterTouristDTO request) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(authService.registerTourist(request));
-        return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body("Successfully registered");
+    @PostMapping("/register/{type}")
+    public ResponseEntity<String> register( @PathVariable String type, @Valid @RequestBody RegisterDTO request) {
+        authService.registerTourist(type, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Successfully registered");
     }
 
-    @PostMapping("/register/guide")
-    public ResponseEntity<String> registerGuide(@RequestBody RegisterGuideDTO request) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(authService.registerGuide(request));
-        return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body("Successfully registered");
-    }
 }
