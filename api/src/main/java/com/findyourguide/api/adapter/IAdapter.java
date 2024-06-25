f817cbe48a2bc90964cf858f76fbf5eb12ebfbf7
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2024-06-25 16:10:08 -0300

    fix beans

--- api/src/main/java/com/findyourguide/api/adapter/IAdapter.java
+++ api/src/main/java/com/findyourguide/api/adapter/IAdapter.java
@@ -1,6 +1,5 @@
 package com.findyourguide.api.adapter;
 
-
 public interface IAdapter {
     void verificate(String credential);
 }
--- api/src/main/java/com/findyourguide/api/adapter/VerificationAdapterX.java
+++ api/src/main/java/com/findyourguide/api/adapter/VerificationAdapterX.java
@@ -1,5 +1,8 @@
 package com.findyourguide.api.adapter;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class VerificationAdapterX implements IAdapter {
     @Override
     public void verificate(String credential) {
